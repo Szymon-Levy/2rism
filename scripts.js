931cbba5b88496f7b8fda26f69a7d649046b1d87
Szymon-Levy <94991990+Szymon-Levy@users.noreply.github.com>
2022-09-05 19:40:26 +0200

    third-day

--- scripts.js
+++ scripts.js
@@ -7,3 +7,24 @@ offcanvasToggler.addEventListener("click", () => {
     offcanvasToggler.classList.toggle("active");
     mainNavigation.classList.toggle("active");
 });
+
+
+//sticky header
+
+const mainHeader = document.getElementById("main-header");
+
+
+window.onscroll = function (){
+    scrollFunction();
+};
+
+function scrollFunction (){
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100 ) {
+        mainHeader.classList.add("sticky");
+    }
+
+    else {
+        mainHeader.classList.remove("sticky");
+    }
+    
+};
